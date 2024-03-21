abbb9ae67a9bb5a0c2f68a31dac58c0af7f35011
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-03-21 16:22:23 +0200

    implementing navLinks

---
+++ src/components/navbar/Logo.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import { buttonVariants } from "../ui/button";
+import { SwitchCamera } from "lucide-react";
+
+export default function Logo() {
+  return (
+    <Link
+      href={"/dashboard"}
+      className={buttonVariants({
+        className:
+          "hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0",
+        variant: "ghost",
+        size: "lg",
+      })}
+    >
+      <SwitchCamera className="h-6 w-6 shrink-0 lg:hidden" />
+      <p
+        className={`font-semibold text-xl hidden lg:block ${"calSans.className"}`}
+      >
+        Instagram
+      </p>
+    </Link>
+  );
+}
---
+++ src/components/navbar/NavLinks.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { links } from "@/data";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        const isActive = pathname === link.href;
+
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={buttonVariants({
+              variant: isActive ? "secondary" : "ghost",
+              className: cn("navLink", { "hidden md:flex": link.hideOnMobile }),
+              size: "lg",
+            })}
+          >
+            <LinkIcon className="w-6" />
+            <p
+              className={`${cn("hidden lg:block", {
+                "font-extrabold": isActive,
+              })}`}
+            >
+              {link.name}
+            </p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
